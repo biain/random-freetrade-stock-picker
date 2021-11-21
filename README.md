16a338ab2dac55d7157b26110533a1d901deef7c
biain <biainbevans@gmail.com>
2021-11-21 12:10:44 +0000

    Add ps to update stock.js. Add 1622 stocks

--- README.md
+++ README.md
@@ -3,6 +3,14 @@ Picks a random stock from the 2,000+ stocks on Freetrade.
 
 List of stocks is based on this list published [here](https://docs.google.com/spreadsheets/d/14Ep-CmoqWxrMU8HshxthRcdRW8IsXvh3n2-ZHVCzqzQ/edit#gid=1855920257).
 
+This list must be maually updated on the stock picker.
+
+### How to update list of stocks
+
+* Download Freetrade-Universe as a csv
+* Run powershell "Update-stocks.ps1" Example: Update-stocks.ps1 -CSVFilePath "C:\temp\Freetrade Investment Universe - Freetrade Universe.csv"
+* Push latest stock.js to master
+
 ## [Live Website](https://biain.github.io/random-freetrade-stock-picker/)
 
 ## Credits 
---
+++ Update-stocks.ps1
@@ -0,0 +1,37 @@
+#requires -version 2
+<#
+.DESCRIPTION
+  Converst a csv export fo stocks to a javascript file.
+.PARAMETER <CSVFilePath>
+  File pathc of the CSV to process.
+.OUTPUTS
+  Outputs stocks.js
+.EXAMPLE
+  Update-stocks.ps1 -CSVFilePath "C:\temp\Freetrade Investment Universe - Freetrade Universe.csv"
+#>
+
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $CSVFilePath
+)
+
+#Set Error Action to Silently Continue
+$ErrorActionPreference = "Stop"
+
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
+
+$CSVFilePath = "D:\Downloads\Freetrade Investment Universe - Freetrade Universe.csv"
+
+[array]$csv = Import-Csv -Path "D:\Downloads\Freetrade Investment Universe - Freetrade Universe.csv"
+
+[array]$Worker = $csv | Select-Object @{Name="Exchange"; Expression = {$($_.'MIC' -split  'x')[1]} }, @{Name="ticker"; Expression = {$_.'Symbol'} }, @{Name="title"; Expression = {$_.'Long_Title'} }
+
+[string]$Worker2 = $Worker | Convertto-json -Compress
+$string = "var stocks = " 
+[string]$Output = ($string + $Worker2)
+
+$Output | Out-File -FilePath "C:\Projects\biain\random-freetrade-stock-picker\stocks.js"
